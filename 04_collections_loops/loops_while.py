bbd5b492f2f62c4cdab9da01cc3471fac7d2c240
Jimmy Madon <hello@jimmymadon.com>
2022-02-05 09:50:48 +0000

    Add extra loop example.

--- 04_collections_loops/loops_while.py
+++ 04_collections_loops/loops_while.py
@@ -7,6 +7,15 @@ while counter < 10:
   print(counter)
   counter = counter + 1
 
+# Use the break statement to break early from a loop
+print("***While loop with condition statement but break early***")
+counter = 0
+while counter < 10:
+  if (counter == 5):
+    break
+  print(counter)
+  counter = counter + 1
+
 print("***While loop with always True statement and break***")
 # Using the break statement at the start of the loop
 counter = 0
